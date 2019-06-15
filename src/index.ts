784e03806c62842a1700da37c61d88123b15f566
陈帅 <qixian.cs@outlook.com>
2019-06-16 00:31:47 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import eslint from "./eslint";
+import stylelint from "./stylelint";
+import prettier from "./prettier";
+
+export { stylelint, prettier };
+
+export default eslint;
---
+++ src/prettier.ts
@@ -0,0 +1,20 @@
+export default {
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 100,
+  proseWrap: "never",
+  overrides: [
+    {
+      files: ".prettierrc",
+      options: {
+        parser: "json"
+      }
+    },
+    {
+      files: "document.ejs",
+      options: {
+        parser: "html"
+      }
+    }
+  ]
+};
