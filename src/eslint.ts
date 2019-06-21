1735379e8b1570540fbddee331191e9e2b33cf2f
duanledexianxianxian <fengyuchenglun@foxmail.com>
2019-06-21 11:31:49 +0800

    fix:区块_mock eslint报错

--- src/eslint.ts
+++ src/eslint.ts
@@ -41,7 +41,7 @@ module.exports = {
       2,
       {
         optionalDependencies: true,
-        devDependencies: ["**/tests/**.js", "/mock/**/**.js", "**/**.test.js"]
+        devDependencies: ["**/tests/**.js", "/mock/**/**.js", "**/**.test.js", '**/**._mock.js']
       }
     ],
     "jsx-a11y/no-noninteractive-element-interactions": 0,
