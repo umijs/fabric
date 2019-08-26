c121d8ab5e0bae9ff5cf3a149803cd4fc3dacd5c
陈帅 <qixian.cs@outlook.com>
2019-08-26 13:53:40 +0800

    close @typescript-eslint/no-non-null-assertion
    边柳和豆酱都觉得有点蠢

--- src/eslint.ts
+++ src/eslint.ts
@@ -1,106 +1,98 @@
-"use strict";
+'use strict';
 
 module.exports = {
   extends: [
-    "airbnb",
-    "prettier",
-    "airbnb-typescript",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:eslint-comments/recommended",
-    "plugin:jest/recommended",
-    "prettier/react",
-    "prettier/@typescript-eslint"
+    'airbnb',
+    'prettier',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:jest/recommended',
+    'prettier/react',
+    'prettier/@typescript-eslint',
   ],
-  plugins: [
-    "@typescript-eslint",
-    "eslint-comments",
-    "jest",
-    "unicorn",
-    "react-hooks"
-  ],
+  plugins: ['@typescript-eslint', 'eslint-comments', 'jest', 'unicorn', 'react-hooks'],
   env: {
     browser: true,
     node: true,
     es6: true,
     mocha: true,
     jest: true,
-    jasmine: true
+    jasmine: true,
   },
   rules: {
-    "react/jsx-wrap-multilines": 0,
-    "react/prop-types": 0,
-    "react/forbid-prop-types": 0,
-    "react/jsx-one-expression-per-line": 0,
-    "generator-star-spacing": 0,
-    "function-paren-newline": 0,
-    "import/no-unresolved": [2, { ignore: ["^@/", "^umi/"] }],
-    "import/order": "warn",
-    "import/no-extraneous-dependencies": [
+    'react/jsx-wrap-multilines': 0,
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'generator-star-spacing': 0,
+    'function-paren-newline': 0,
+    'import/no-unresolved': [2, { ignore: ['^@/', '^umi/'] }],
+    'import/order': 'warn',
+    'import/no-extraneous-dependencies': [
       2,
       {
         optionalDependencies: true,
         devDependencies: [
-          "**/tests/**.{ts,js,jsx,tsx}",
-          "**/_test_/**.{ts,js,jsx,tsx}",
-          "/mock/**/**.{ts,js,jsx,tsx}",
-          "**/**.test.{ts,js,jsx,tsx}",
-          "**/_mock.{ts,js,jsx,tsx}",
-          "**/example/**.{ts,js,jsx,tsx}",
-          "**/examples/**.{ts,js,jsx,tsx}"
-        ]
-      }
+          '**/tests/**.{ts,js,jsx,tsx}',
+          '**/_test_/**.{ts,js,jsx,tsx}',
+          '/mock/**/**.{ts,js,jsx,tsx}',
+          '**/**.test.{ts,js,jsx,tsx}',
+          '**/_mock.{ts,js,jsx,tsx}',
+          '**/example/**.{ts,js,jsx,tsx}',
+          '**/examples/**.{ts,js,jsx,tsx}',
+        ],
+      },
     ],
-    "jsx-a11y/no-noninteractive-element-interactions": 0,
-    "jsx-a11y/click-events-have-key-events": 0,
-    "jsx-a11y/no-static-element-interactions": 0,
-    "jsx-a11y/anchor-is-valid": 0,
-    "linebreak-style": 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'linebreak-style': 0,
     // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
-    "no-prototype-builtins": "off",
-    "import/prefer-default-export": "off",
-    "import/no-default-export": [0, "camel-case"],
+    'no-prototype-builtins': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': [0, 'camel-case'],
     // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-    "react/destructuring-assignment": "off",
-    "react/jsx-filename-extension": "off",
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': 'off',
     // Use function hoisting to improve code readability
-    "no-use-before-define": [
-      "error",
-      { functions: false, classes: true, variables: true }
-    ],
+    'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
     // Makes no sense to allow type inferrence for expression parameters, but require typing the response
-    "@typescript-eslint/explicit-function-return-type": [
-      "off",
-      { allowTypedFunctionExpressions: true }
+    '@typescript-eslint/explicit-function-return-type': [
+      'off',
+      { allowTypedFunctionExpressions: true },
     ],
-    "@typescript-eslint/no-use-before-define": [
-      "error",
-      { functions: false, classes: true, variables: true, typedefs: true }
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true, typedefs: true },
     ],
-    "@typescript-eslint/no-var-requires": 0,
-
+    '@typescript-eslint/no-var-requires': 0,
     // Common abbreviations are known and readable
-    "unicorn/prevent-abbreviations": "off",
-    "@typescript-eslint/explicit-member-accessibility": 0,
-    "@typescript-eslint/interface-name-prefix": 0,
-    "import/no-cycle": 0,
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    'unicorn/prevent-abbreviations': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+
+    'import/no-cycle': 0,
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
 
     // issue https://github.com/facebook/react/issues/15204
-    "react-hooks/exhaustive-deps": "off", // Checks effect dependencies
+    'react-hooks/exhaustive-deps': 'off', // Checks effect dependencies
 
     // Conflict with prettier
-    "arrow-body-style": ["error", "as-needed"],
-    "arrow-parens": 1,
-    "object-curly-newline": 0,
-    "implicit-arrow-linebreak": 0,
-    "operator-linebreak": 0,
-    "eslint-comments/no-unlimited-disable": 1,
-    "no-param-reassign": 1,
-    "space-before-function-paren": 0
+    'arrow-body-style': ['error', 'as-needed'],
+    'arrow-parens': 1,
+    'object-curly-newline': 0,
+    'implicit-arrow-linebreak': 0,
+    'operator-linebreak': 0,
+    'eslint-comments/no-unlimited-disable': 1,
+    'no-param-reassign': 1,
+    'space-before-function-paren': 0,
   },
   settings: {
     // support import modules from TypeScript files in JavaScript files
-    "import/resolver": { node: { extensions: [".js", ".jsx", ".ts", ".tsx"] } },
-    polyfills: ["fetch", "Promise", "URL", "object-assign"]
-  }
+    'import/resolver': { node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] } },
+    polyfills: ['fetch', 'Promise', 'URL', 'object-assign'],
+  },
 };
--- src/softyEslint.ts
+++ src/softyEslint.ts
@@ -1,96 +1,94 @@
-"use strict";
+'use strict';
 
 module.exports = {
   extends: [
-    "airbnb",
-    "prettier",
-    "airbnb-typescript",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jest/recommended",
-    "prettier/react",
-    "prettier/@typescript-eslint"
+    'airbnb',
+    'prettier',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier/react',
+    'prettier/@typescript-eslint',
   ],
-  plugins: ["@typescript-eslint", "jest", "unicorn", "react-hooks"],
+  plugins: ['@typescript-eslint', 'jest', 'unicorn', 'react-hooks'],
   env: {
     browser: true,
     node: true,
     es6: true,
     mocha: true,
     jest: true,
-    jasmine: true
+    jasmine: true,
   },
   rules: {
-    "react/jsx-wrap-multilines": 0,
-    "react/prop-types": 0,
-    "react/forbid-prop-types": 0,
-    "react/jsx-one-expression-per-line": 0,
-    "react/sort-comp": 0,
+    'react/jsx-wrap-multilines': 0,
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/sort-comp': 0,
 
-    "generator-star-spacing": 0,
-    "function-paren-newline": 0,
-    "import/no-unresolved": [1, { ignore: ["^@/", "^umi/"] }],
-    "import/no-extraneous-dependencies": [
+    'generator-star-spacing': 0,
+    'function-paren-newline': 0,
+    'import/no-unresolved': [1, { ignore: ['^@/', '^umi/'] }],
+    'import/no-extraneous-dependencies': [
       1,
       {
         optionalDependencies: true,
         devDependencies: [
-          "**/tests/**..{ts,js,jsx,tsx}",
-          "**/_test_/**.{ts,js,jsx,tsx}",
-          "/mock/**/**.{ts,js,jsx,tsx}",
-          "**/**.test.{ts,js,jsx,tsx}",
-          "**/_mock.{ts,js,jsx,tsx}"
-        ]
-      }
+          '**/tests/**..{ts,js,jsx,tsx}',
+          '**/_test_/**.{ts,js,jsx,tsx}',
+          '/mock/**/**.{ts,js,jsx,tsx}',
+          '**/**.test.{ts,js,jsx,tsx}',
+          '**/_mock.{ts,js,jsx,tsx}',
+        ],
+      },
     ],
-    "jsx-a11y/no-noninteractive-element-interactions": 0,
-    "jsx-a11y/click-events-have-key-events": 0,
-    "jsx-a11y/no-static-element-interactions": 0,
-    "jsx-a11y/anchor-is-valid": 0,
-    "linebreak-style": 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'linebreak-style': 0,
     // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
-    "no-prototype-builtins": "off",
-    "import/prefer-default-export": "off",
-    "import/no-default-export": [0, "camel-case"],
+    'no-prototype-builtins': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': [0, 'camel-case'],
     // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-    "react/destructuring-assignment": "off",
-    "react/jsx-filename-extension": "off",
-    "max-len": "warn",
-    "sort-imports": 0,
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': 'off',
+    'max-len': 'warn',
+    'sort-imports': 0,
     // Use function hoisting to improve code readability
-    "no-use-before-define": [
-      "warn",
-      { functions: false, classes: true, variables: true }
-    ],
+    'no-use-before-define': ['warn', { functions: false, classes: true, variables: true }],
     // Makes no sense to allow type inferrence for expression parameters, but require typing the response
-    "@typescript-eslint/explicit-function-return-type": [
-      "off",
-      { allowTypedFunctionExpressions: true }
+    '@typescript-eslint/explicit-function-return-type': [
+      'off',
+      { allowTypedFunctionExpressions: true },
     ],
-    "@typescript-eslint/camelcase": "off",
-    "@typescript-eslint/no-var-requires": 0,
-    "@typescript-eslint/no-use-before-define": [
-      "warn",
-      { functions: false, classes: true, variables: true, typedefs: true }
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-use-before-define': [
+      'warn',
+      { functions: false, classes: true, variables: true, typedefs: true },
     ],
     // Common abbreviations are known and readable
-    "unicorn/prevent-abbreviations": "off",
-    "@typescript-eslint/explicit-member-accessibility": 0,
-    "@typescript-eslint/interface-name-prefix": 0,
-    "import/no-cycle": 0,
-    "react-hooks/rules-of-hooks": "warn", // Checks rules of Hooks
-    "react/no-array-index-key": "warn",
+    'unicorn/prevent-abbreviations': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    'import/no-cycle': 0,
+    'react-hooks/rules-of-hooks': 'warn', // Checks rules of Hooks
+    'react/no-array-index-key': 'warn',
     // issue https://github.com/facebook/react/issues/15204
-    "react-hooks/exhaustive-deps": "off", // Checks effect dependencies
+    'react-hooks/exhaustive-deps': 'off', // Checks effect dependencies
 
     // Conflict with prettier
-    "arrow-body-style": ["warn", "as-needed"],
-    "object-curly-newline": 0,
-    "implicit-arrow-linebreak": 0,
-    "operator-linebreak": 0,
-    "no-mixed-operators": "warn",
-    "comma-dangle": 0
+    'arrow-body-style': ['warn', 'as-needed'],
+    'object-curly-newline': 0,
+    'implicit-arrow-linebreak': 0,
+    'operator-linebreak': 0,
+    'no-mixed-operators': 'warn',
+    'comma-dangle': 0,
   },
   settings: {
-    "import/resolver": { node: { extensions: [".js", ".jsx", ".ts", ".tsx"] } }
-  }
+    'import/resolver': { node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] } },
+  },
 };
