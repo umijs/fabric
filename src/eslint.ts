5f16fbbdc93690fc6928cf657832ca3c66c78f44
陈帅 <qixian.cs@outlook.com>
2019-10-20 21:27:44 +0800

    support eslint 6 (#13)
    * v2.0.0-beta.2
    
    * support require.resolve
    
    * v2.0.0-beta.3

--- src/eslint.ts
+++ src/eslint.ts
@@ -1,4 +1,13 @@
-'use strict';
+import * as path from 'path';
+
+const eslintFolder = path.join(path.dirname(require.resolve('eslint')), '..');
+
+const moduleResolverPath = path.join(eslintFolder, 'lib/shared/relative-module-resolver');
+const ModuleResolver = require(moduleResolverPath);
+
+ModuleResolver.resolve = function(moduleName: string) {
+  return require.resolve(moduleName);
+};
 
 module.exports = {
   extends: [
--- src/softyEslint.ts
+++ src/softyEslint.ts
@@ -1,5 +1,16 @@
 'use strict';
 
+import * as path from 'path';
+
+const eslintFolder = path.join(path.dirname(require.resolve('eslint')), '..');
+
+const moduleResolverPath = path.join(eslintFolder, 'lib/shared/relative-module-resolver');
+const ModuleResolver = require(moduleResolverPath);
+
+ModuleResolver.resolve = function(moduleName: string) {
+  return require.resolve(moduleName);
+};
+
 module.exports = {
   extends: [
     'airbnb',
