522cb483a328b89ac5e13f96dab9b67bff5f46eb
陈帅 <qixian.cs@outlook.com>
2020-06-19 18:17:31 +0800

    Update stylelint.ts (#43)

--- src/stylelint.ts
+++ src/stylelint.ts
@@ -2,21 +2,23 @@
 
 module.exports = {
   extends: [
-    'stylelint-config-standard',
-    'stylelint-config-css-modules',
-    'stylelint-config-rational-order',
-    'stylelint-config-prettier',
+    "stylelint-config-standard",
+    "stylelint-config-css-modules",
+    "stylelint-config-rational-order",
+    "stylelint-config-prettier",
   ].map((key) => require.resolve(key)),
-  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'].map((key) =>
-    require.resolve(key)
-  ),
+  plugins: [
+    "stylelint-order",
+    "stylelint-declaration-block-no-ignored-properties",
+  ].map((key) => require.resolve(key)),
   rules: {
-    'no-descending-specificity': null,
+    "no-descending-specificity": null,
     //https://github.com/stylelint/stylelint/issues/4114
-    'function-calc-no-invalid': null,
-    'font-family-no-missing-generic-family-keyword': null, // iconfont
-    'plugin/declaration-block-no-ignored-properties': true,
-    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
+    "function-calc-no-invalid": null,
+    "function-url-quotes": "always",
+    "font-family-no-missing-generic-family-keyword": null, // iconfont
+    "plugin/declaration-block-no-ignored-properties": true,
+    "unit-no-unknown": [true, { ignoreUnits: ["rpx"] }],
   },
-  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
+  ignoreFiles: ["**/*.js", "**/*.jsx", "**/*.tsx", "**/*.ts"],
 };
