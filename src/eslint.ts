a5ca5dd3bc23ad2fed7c33a6f799d183a85028c0
chenshuai2144 <qixian.cs@outlook.com>
2020-07-06 13:26:23 +0800

    add commet

--- src/eslint.ts
+++ src/eslint.ts
@@ -15,6 +15,15 @@ if (!fs.existsSync(path.join(process.env.PWD || '.', './tsconfig.json'))) {
   parserOptions = {
     tsconfigRootDir: __dirname,
     project: './tsconfig.json',
+    /**
+     * parserOptions.createDefaultProgram
+     * Default .false
+     * This option allows you to request that when the setting is specified,
+     * files will be allowed when not included in the projects defined by the provided files.
+     * Using this option will incur significant performance costs.
+     * This option is primarily included for backwards-compatibility.
+     * See the project section above for more information.projecttsconfig.json
+     */
     createDefaultProgram: true,
   };
 }
