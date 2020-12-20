ad48fa51fd604b5cb0d8836ac8b2d9b200f76854
chenshuai2144 <qixian.cs@outlook.com>
2020-12-20 20:27:20 +0800

    fix ts

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,7 +1,7 @@
 const path = require('path');
-
+console.log(require.resolve(path.join(__dirname, '../', 'dist/eslint')));
 module.exports = {
-  extends: [require.resolve(path.join(__dirname, '../dist/eslint'))],
+  extends: [require.resolve(path.join(__dirname, '../', 'dist/eslint'))],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
