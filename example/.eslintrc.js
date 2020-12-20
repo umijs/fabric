2dbc3d9db80fb030a6565d51f09eff185e926872
chenshuai2144 <qixian.cs@outlook.com>
2020-12-20 20:15:48 +0800

    add ts config

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,5 +1,7 @@
+const path = require('path');
+
 module.exports = {
-  extends: [require.resolve('../dist/eslint')],
+  extends: [require.resolve(path.join(__dirname, '../dist/eslint'))],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
