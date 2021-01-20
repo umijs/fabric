16f831d985acc03faced728fb628e27b2fe53809
chenshuai2144 <qixian.cs@outlook.com>
2021-01-20 17:46:04 +0800

    support plugin-proposal-decorators

--- src/eslint.ts
+++ src/eslint.ts
@@ -8,7 +8,10 @@ const parserOptions = {
   },
   babelOptions: {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-class-properties'],
+    plugins: [
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ],
   },
   requireConfigFile: false,
   project: './tsconfig.json',
