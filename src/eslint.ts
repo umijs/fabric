67fb040821d64e0ca32500ca058398cc66c7000d
chenshuai2144 <qixian.cs@outlook.com>
2021-01-20 20:09:45 +0800

    support ts in jsconfig

--- src/eslint.ts
+++ src/eslint.ts
@@ -7,7 +7,7 @@ const parserOptions = {
     jsx: true,
   },
   babelOptions: {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
+    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
       ['@babel/plugin-proposal-class-properties', { loose: true }],
