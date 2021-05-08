5242b9886496ee7964ce79e937ce18482699c66d
chenshuai2144 <qixian.cs@outlook.com>
2021-05-08 18:24:59 +0800

    support HUSKY_GIT_PARAMS

--- src/verifyCommit.ts
+++ src/verifyCommit.ts
@@ -4,7 +4,7 @@
 const chalk = require('chalk');
 const osLocale = require('os-locale');
 
-const msgPath = process.env.GIT_PARAMS;
+const msgPath = process.env.GIT_PARAMS || process.env.HUSKY_GIT_PARAMS;
 const msg = require('fs').readFileSync(msgPath, 'utf-8').trim();
 
 const commitRE = /^(((\ud83c[\udf00-\udfff])|(\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55]) )?(revert: )?(feat|fix|docs|UI|refactor|⚡perf|workflow|build|CI|typos|chore|tests|types|wip|release|dep|locale)(\(.+\))?: .{1,50}/;
