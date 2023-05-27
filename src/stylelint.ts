a071c01dedb5214cb992988d073def909c19a3e3
期贤 <qixian.cs@antgroup.com>
2023-05-28 00:02:26 +0800

    up to latest verison

--- src/stylelint.ts
+++ src/stylelint.ts
@@ -11,12 +11,22 @@ module.exports = {
     'no-descending-specificity': null,
     'function-url-quotes': 'always',
     'selector-attribute-quotes': 'always',
+    'selector-class-pattern': null,
     'font-family-no-missing-generic-family-keyword': null, // iconfont
     'plugin/declaration-block-no-ignored-properties': true,
     'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
-    // webcomponent
     'selector-type-no-unknown': null,
     'value-keyword-case': ['lower', { ignoreProperties: ['composes'] }],
   },
+  overrides: [
+    {
+      files: ['*.scss', '**/*.scss'],
+      customSyntax: 'postcss-scss',
+    },
+    {
+      files: ['*.less', '**/*.less'],
+      customSyntax: 'postcss-less',
+    },
+  ],
   ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
 };
